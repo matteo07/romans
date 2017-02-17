85bd7f1aedd11a13f755a82b369969fab0c49a2b
dema <a@.it>
2017-02-17 14:20:10 +0100

    created roman to arab

---
+++ test/unit/roman_to_arab_test.rb
@@ -0,0 +1,13 @@
+require_relative '../test_case'
+require_relative '../../lib/tasks/roman_to_arab'
+
+class FizzBuzzTest < Test::Unit::TestCase
+  def setup
+    @romans = RomanToArab.new
+  end
+
+  def test_ones
+    assert_equal 1, 1
+  end
+
+end
