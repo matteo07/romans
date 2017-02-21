f7a7ffa432cbb4d692e2ff27d202f66990578513
dema <a@.it>
2017-02-21 17:49:34 +0100

    added controls in boot

--- lib/boot.rb
+++ lib/boot.rb
@@ -1,4 +1,6 @@
 require_relative '../lib/tasks/arab_roman'
+require_relative '../lib/exceptions/invalid_character_exception'
+require_relative '../lib/exceptions/invalid_order_exception'
 
 while true
   puts "Write your number to convert (q to exit)"
@@ -8,8 +10,10 @@ while true
   end
   begin
     puts input + " converted is " + ArabRoman.convert(input).to_s
-  rescue
-    puts input + " is not a valid roman number format"
+  rescue InvalidOrderException
+    puts input + " has roman numbers in the wrong order"
+  rescue InvalidCharacterException
+    puts input + " contains characters that are not roman numbers"
   end
 end
 p 'Bye'
